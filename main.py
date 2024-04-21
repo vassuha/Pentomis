2fcd63a92bca55117d48f406210e5a7dce6ea0ab
suhar <vasuharn@gmail.com>
2024-04-22 00:08:00 +0300

    Добавлен базовый графический интерфейс, текстурированы фигуры

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ import copy
 from random import choice
 
 pygame.init()
-screen = pygame.display.set_mode((1920, 1080))
+screenWidth = 1920-500
+screenHeight = 1080-300
+
+areaWidth = 10
+areaHeight = 20
+TEXTURE_WIDTH = 54
+screen = pygame.display.set_mode((screenWidth, screenHeight), pygame.RESIZABLE)
 
 class Figure:
     def __init__(self, name, area, form, rotationAngle, position, color, center):
@@ -20,7 +26,7 @@ class Figure:
     def rotate(self, area):
         for i in range(len(self.form)):
             for j in range(len(self.form)):
-                if (self.form[i][j] == 1):
+                if (self.form[i][j] > 0):
                     area[self.position[0] + i][self.position[1] + j] = 0
         tempForm = copy.deepcopy(self.form)
         n = len(tempForm)
@@ -40,17 +46,19 @@ class Figure:
 
         for i in range(len(self.form)):
             for j in range(len(self.form)):
-                if area[self.position[0] + i][self.position[1] + j] + self.form[i][j] >1:
+                if (area[self.position[0] + i][self.position[1] + j] >0) and (self.form[i][j] >0):
                     self.position = copy.deepcopy(positionOld)
                     self.form = copy.deepcopy(tempForm)
                     self.center = copy.deepcopy(centerOld)
                     for i in range(len(self.form)):
                         for j in range(len(self.form)):
-                            area[self.position[0] + i][self.position[1] + j] += self.form[i][j]
+                            if self.form[i][j] > 0:
+                                area[self.position[0] + i][self.position[1] + j] = self.form[i][j]
                     return False
         for i in range(len(self.form)):
             for j in range(len(self.form)):
-                area[self.position[0] + i][self.position[1] + j] += self.form[i][j]
+                if self.form[i][j] > 0:
+                    area[self.position[0] + i][self.position[1] + j] = self.form[i][j]
 
 
     def move(self, area, dir):
@@ -59,38 +67,40 @@ class Figure:
                 return False
             for i in range(len(self.form)):
                 for j in range(len(self.form)):
-                    if(self.form[i][j] == 1):
+                    if(self.form[i][j] > 0):
                         area[self.position[0] + i][self.position[1] + j] = 0
             for i in range(len(self.form)):
                 for j in range(len(self.form)):
-                    if area[self.position[0] + i][self.position[1] + j + 1] + self.form[i][j] > 1 or (self.form[i][j] == 1 and (self.position[1]+j) > 12):
+                    if ((area[self.position[0] + i][self.position[1] + j + 1] > 0) and (self.form[i][j] > 0)) or (self.form[i][j] > 0 and (self.position[1]+j) > 12):
                         for i in range(len(self.form)):
                             for j in range(len(self.form)):
-                                if (self.form[i][j] == 1):
+                                if (self.form[i][j] > 0):
                                     area[self.position[0] + i][self.position[1] + j] = self.form[i][j]
                         return False
             for i in range(len(self.form)):
                 for j in range(len(self.form)):
-                    area[self.position[0] + i][self.position[1] + j + 1] += self.form[i][j]
+                    if self.form[i][j] > 0:
+                        area[self.position[0] + i][self.position[1] + j + 1] = self.form[i][j]
             self.position[1] += 1
         if dir == "left":
             if self.position[1] < 1:
                 return False
             for i in range(len(self.form)):
                 for j in range(len(self.form)):
-                    if(self.form[i][j] == 1):
+                    if(self.form[i][j] > 0):
                         area[self.position[0] + i][self.position[1] + j] = 0
             for i in range(len(self.form)):
                 for j in range(len(self.form)):
-                    if area[self.position[0] + i][self.position[1] + j - 1] + self.form[i][j] > 1 or (self.form[i][j] == 1 and (self.position[1]+j) < 5):
+                    if ((area[self.position[0] + i][self.position[1] + j - 1] > 0) and (self.form[i][j] > 0)) or (self.form[i][j] > 0 and (self.position[1]+j) < 5):
                         for i in range(len(self.form)):
                             for j in range(len(self.form)):
-                                if (self.form[i][j] == 1):
+                                if (self.form[i][j] > 0):
                                     area[self.position[0] + i][self.position[1] + j] = self.form[i][j]
                         return False
             for i in range(len(self.form)):
                 for j in range(len(self.form)):
-                    area[self.position[0] + i][self.position[1] + j - 1] += self.form[i][j]
+                    if self.form[i][j] > 0:
+                        area[self.position[0] + i][self.position[1] + j - 1] = self.form[i][j]
             self.position[1] -= 1
         if dir == "down":
             if self.position[0] + len(self.form) > len(area) - 1:
@@ -98,19 +108,20 @@ class Figure:
 
             for i in range(len(self.form)):
                 for j in range(len(self.form)):
-                    if(self.form[i][j] == 1):
+                    if(self.form[i][j] > 0):
                         area[self.position[0] + i][self.position[1] + j] = 0
             for i in range(len(self.form)):
                 for j in range(len(self.form)):
-                    if area[self.position[0] + i + 1][self.position[1] + j] + self.form[i][j] > 1 or (self.form[i][j] == 1 and (self.position[0]+i) > 22):
+                    if ((area[self.position[0] + i + 1][self.position[1] + j] >0) and (self.form[i][j] > 0)) or (self.form[i][j] > 0 and (self.position[0]+i) > 22):
                         for i in range(len(self.form)):
                             for j in range(len(self.form)):
-                                if (self.form[i][j] == 1):
+                                if (self.form[i][j] > 0):
                                     area[self.position[0] + i][self.position[1] + j] = self.form[i][j]
                         return False
             for i in range(len(self.form)):
                 for j in range(len(self.form)):
-                    area[self.position[0] + i + 1][self.position[1] + j] += self.form[i][j]
+                    if self.form[i][j] > 0:
+                        area[self.position[0] + i + 1][self.position[1] + j] = self.form[i][j]
             self.position[0] += 1
             return True
 
@@ -129,9 +140,9 @@ def spawn(area, figure):
 def checkCollision(area, figure):
     for i in range(len(figure.form)):
         for j in range(len(figure.form)):
-            if figure.form[i][j] == 1  and area[figure.position[0] + i+1][figure.position[1] + j] > 0:
+            if figure.form[i][j] > 0  and area[figure.position[0] + i+1][figure.position[1] + j] > 0:
                 if i < len(figure.form)-1:
-                    if figure.form[i+1][j] != 1:
+                    if figure.form[i+1][j] == 0:
                         return True
                 else:
                     return True
@@ -158,7 +169,7 @@ def checkLine(area, score):
 
 def checkEnd(area):
     for j in range(4, 14):
-        if area[4][j] == 1:
+        if area[4][j] > 0:
             return True
 
 
@@ -178,19 +189,19 @@ pointForm= [[0, 0, 0], [0, 0, 0], [1, 0, 0]]
 pointFigure = Figure("point", area, pointForm, 0, (0, 0), "red", [2, 0])
 figures.append(pointFigure)
 
-blueForm = [[0, 0, 0], [1, 0, 0], [1, 1, 1]]
+blueForm = [[0, 0, 0], [3, 0, 0], [3, 3, 3]]
 blueFigure = Figure("blue", area, blueForm, 0, (0, 0), "blue", [2, 1])
 figures.append(blueFigure)
 
-greenForm = [[0, 0, 0], [0, 1, 1], [1, 1, 0]]
+greenForm = [[0, 0, 0], [0, 6, 6], [6, 6, 0]]
 greenFigure = Figure("green", area, greenForm, 0, (0, 0), "green", [1, 1])
 figures.append(greenFigure)
 
-blackForm = [[1, 1, 0], [1, 1, 0], [1, 1, 0]]
+blackForm = [[18, 18, 0], [18, 18, 0], [18, 18, 0]]
 blackFigure = Figure("black", area, blackForm, 0, (0, 0), "black", [1, 1])
 figures.append(blackFigure)
 
-lineForm = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1]]
+lineForm = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [2, 2, 2, 2]]
 lineFigure = Figure("line", area, lineForm, 0, (0, 0), "purple", [3, 2])
 figures.append(lineFigure)
 
@@ -212,13 +223,10 @@ for i in range(28):
 isMove = False
 
 def timer(score=0):
-    t=250-score*10
-    if t<100:
-        t = 100
+    t=900-score*10
+    if t<75:
+        t = 75
     return t
-
-
-#def gameplay1(area)
 
 
 MOVEMENT, T= pygame.USEREVENT, timer()
@@ -230,6 +238,32 @@ except FileNotFoundError:
     f = open("record.txt", "w")
     f.write("0" + "\n")
 
+blocks = []
+for i in range(1, 20):
+    blocks.append(pygame.image.load("img/textures/Pentomis_texture_" + str(i) + ".png").convert())
+def renderGameplay(area, background, score, blocks):
+    bg = pygame.transform.scale(pygame.image.load(background).convert(), (screenWidth, screenHeight))
+
+    blockHeight = screenHeight/areaHeight*0.9
+    border = screenHeight//(270)
+
+    screen.blit(bg, (0, 0)) #вывод
+    pygame.draw.rect(screen, (255, 0, 0), (screenWidth/2-blockHeight*(areaWidth/2-1)-border/2, screenHeight/2-blockHeight*(areaHeight/2)-border/2, blockHeight*areaWidth+border, blockHeight*(areaHeight+1)+border/2), border)
+
+    for i in range(3, 24):
+        for j in range(4, 14):
+            #Отладкаааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааа
+            if area[i][j] >0:
+                screen.blit(pygame.transform.scale(blocks[area[i][j]], (blockHeight, blockHeight)), (screenWidth/2+(j-8)*blockHeight, (i-2)*blockHeight))
+    pygame.display.update()
+
+
+
+def renderStartMenu(width, height):
+    background = pygame.transform.scale(pygame.image.load("img/background/start.png").convert(), (width, height))
+    screen.blit(background, (0, 0))
+    menu = pygame.draw.rect(surface, color, pygame.Rect(30, 30, 60, 60),  2, 3)
+    screen.blit(menu, (0, 0))
 
 while running:
     #Создание игрового поля
@@ -241,6 +275,12 @@ while running:
     area[24] = [1] * 18
 
     score = 0
+    bgColor = 'Black'
+    screen.fill(bgColor)
+
+    tempFigure = choice(figures)
+    nextFigure = choice(figures)
+    spawn(area, tempFigure)
 
     #Стартовое меню
     gameplay = False
@@ -276,15 +316,18 @@ while running:
             tempFigure.rotate(area)
 
         #Отрисовка
-        screen.fill(bgColor)
-        for i in range(3, 24):
-            s = str(area[i][4:14])
-            #s = s.replace("1", "@");
-            s = s.replace("1", "  ");
-            text1[i] = myFont.render(s, True, "White")
-            screen.blit(text1[i], (0, i*20))
-
-        pygame.display.update()
+        # screen.fill(bgColor)
+        # for i in range(3, 24):
+        #     s = str(area[i][4:14])
+        #     #s = s.replace("1", "@");
+        #     s = s.replace("2", "  ");
+        #     text1[i] = myFont.render(s, True, "White")
+        #     screen.blit(text1[i], (0, i*20))
+        #
+        # pygame.display.update()
+        screenWidth = screen.get_size()[0]
+        screenHeight = screen.get_size()[1]
+        renderGameplay(area, "img/backgrounds/Minimalistic_landscape_1.jpg", score, blocks)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
@@ -296,8 +339,10 @@ while running:
                     score = checkLine(area, score)
                     if checkEnd(area):
                         gameplay = False
-                    tempFigure = choice(figures)
+                    tempFigure = nextFigure
+                    nextFigure = choice(figures)
                     spawn(area, tempFigure)
+                    isMove = False
                     pygame.display.update()
                 tempFigure.move(area, "down")
                 pygame.display.update()
@@ -315,6 +360,7 @@ while running:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_DOWN:
                     isMove = False
+
 
         clock.tick(60)
 
